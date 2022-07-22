e13214455926f6de8f0a44b43e09f4b7f08e55aa
Artem <67517973+KingArthur-IT@users.noreply.github.com>
2022-07-22 23:12:33 +0300

    burger

--- script/burger.js
+++ script/burger.js
@@ -1,5 +1,15 @@
-document.querySelector('.header__burger').addEventListener('click', () => {
-    document.querySelector('.header__burger').classList.toggle('burger-opened');
-    document.querySelector('.burger-menu').classList.toggle('opened');
-    document.querySelector('body').classList.toggle('overflow-hidden');
-})
+const headerBurger = document.querySelector('.header__burger');
+if (headerBurger)
+    document.querySelector('.header__burger').addEventListener('click', () => {
+        document.querySelector('.header__burger').classList.toggle('burger-opened');
+        document.querySelector('.burger-menu').classList.toggle('opened');
+        document.querySelector('body').classList.toggle('overflow-hidden');
+    })
+
+const blogHeaderBurger = document.querySelector('.blog-header__burger');
+if (blogHeaderBurger)
+    document.querySelector('.blog-header__burger').addEventListener('click', () => {
+        document.querySelector('.blog-header__burger').classList.toggle('burger-opened');
+        document.querySelector('.burger-menu').classList.toggle('opened');
+        document.querySelector('body').classList.toggle('overflow-hidden');
+    })
--- script/menu.js
+++ script/menu.js
@@ -1,6 +1,6 @@
 window.addEventListener('scroll', animMenuOnScroll);
 function animMenuOnScroll() {
-    //mewnu fixed
+    //menu fixed
     const isMenuFixed = document.getElementById('header-nav').classList.contains('fixed');
     if (window.pageYOffset > 100 && !isMenuFixed)
         document.getElementById('header-nav').classList.add('fixed');
